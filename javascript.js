d1011b66d6baf310b76ea9c4d5a8faac25e07931
Ramolo Berutu <92324195+ramoloberutu@users.noreply.github.com>
2021-11-13 02:04:41 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Calculator</title>
+ <style type="text/css">
+  .tombol{
+   background-color: deepskyblue;
+  }
+  .tabel{
+   background-color: gold;
+   margin: 10px;
+   padding: 10px;
+  }
